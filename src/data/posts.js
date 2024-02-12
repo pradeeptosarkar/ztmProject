5c236f3e2b2314dfce44007c2340c3ae9ac21b48
Pradeepto Sarkar <psrcofficial10@gmail.com>
2024-02-12 09:33:59 +0530

    root

---
+++ src/data/posts.js
@@ -0,0 +1,30 @@
+import forest from "../../assets/images/forest.jpg";
+import sea from "../../assets/images/sea.jpg";
+import house from "../../assets/images/house.jpg";
+
+export const posts = [
+  {
+    id: 1,
+    photo: forest,
+    title: "Ліс",
+    comments: 8,
+    likes: 153,
+    country: "Ukraine",
+  },
+  {
+    id: 2,
+    photo: sea,
+    title: "Захід на Чорному морі",
+    comments: 3,
+    likes: 200,
+    country: "Ukraine",
+  },
+  {
+    id: 3,
+    photo: house,
+    title: "Старий будиночок у Венеції",
+    comments: 50,
+    likes: 200,
+    country: "Italy",
+  },
+];
